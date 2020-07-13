8239ea0b7db27f6cef73dab8ad6334590d9a1e73
Sihoo-milk <>
2020-07-13 21:24:04 +0900

    クライアント修正。バーガーメニュー設置

--- js/common.js
+++ js/common.js
@@ -13,23 +13,14 @@ $(function() {
 });
 
 // スムーススクロールの設定
-$(document).ready(function () {
-  $("#goto_top").hide();
-  $(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        $('#goto_top').fadeIn();
-      } else {
-        $('#goto_top').fadeOut();
-      }
-    });
-
-    $('#goto_top a').click(function () {
-      $('body,html').animate({
-        scrollTop: 0
-      }, 800);
-      return false;
-    });
+$(function() {
+  $('a[href^="#"]').click(function() {
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
   });
 });
 
@@ -44,3 +35,17 @@ $(function() {
     $('#popup, #layer').hide();
   });
 });
+
+function toggleNav() {
+  var body = document.body;
+  var hamburger = document.getElementById('js-hamburger');
+  var blackBg = document.getElementById('js-black-bg');
+
+  hamburger.addEventListener('click', function() {
+    body.classList.toggle('nav-open');
+  });
+  blackBg.addEventListener('click', function() {
+    body.classList.remove('nav-open');
+  });
+}
+toggleNav();
